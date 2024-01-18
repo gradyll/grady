bf2cf41001a340398db524367fdc511a01ca76fa
gradyll <gradyll@users.noreply.github.com>
2024-01-18 02:39:07 +0000

    deploy: 3827c287a89da4f75f7a688f2448e8c06b1ba40d

--- assets/life_index.md.58eba886.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as s,c as e,Q as n}from"./chunks/framework.64a3e084.js";const u=JSON.parse('{"title":"生活随录ssss","description":"","frontmatter":{},"headers":[],"relativePath":"life/index.md","lastUpdated":1704698440000}'),l={name:"life/index.md"},t=n("",5),o=[t];function p(c,i,r,d,h,C){return s(),e("div",null,o)}const y=a(l,[["render",p]]);export{u as __pageData,y as default};
