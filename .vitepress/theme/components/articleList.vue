31c4cd2310b988300a34cc27efc046ec24aec653
ymiandi <571785481@qq.com>
2024-03-20 10:16:30 +0800

    掘金稳住
    掘金稳住

--- .vitepress/theme/components/articleList.vue
+++ .vitepress/theme/components/articleList.vue
@@ -1,7 +1,7 @@
 <template >
   <div class="wrapper" v-for="(item, index) in articleList" :key="index">
     <div class="article">
-      <div class="title">标题{{ index+ 1 }}: <span><a :href="item.href">{{ item.title }}</a></span></div>
+      <div class="title">标题{{ index+ 1 }}: <span><a :href="item.href" target="_blank">{{ item.title }}</a></span></div>
     </div>
   </div>
 </template>
